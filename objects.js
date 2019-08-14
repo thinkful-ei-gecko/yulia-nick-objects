adbee93f80a8e30ff468bdc8f208014238c1dd62
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-08-14 09:16:19 -0700

    updateObject

--- objects.js
+++ objects.js
@@ -1,11 +1,23 @@
+'use strict';
 function createMyObject(){
-    return {
-     foo: 'bar',
-     answerToUniverse: 42,
-     'olly olly': 'oxen free',
-     sayHello() {return 'hello'}
-    }
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {return 'hello';}
+  };
 }
 const testOBJ =createMyObject();
 
-console.log(testOBJ);
+//console.log(testOBJ);
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz ='bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+const testObj = {};
+
+//console.log(updateObject(testObj));
