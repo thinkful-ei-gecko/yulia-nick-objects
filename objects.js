95ecf157ce96d31cc8a1b638257404421282901f
najc33 <najcoletta@gmail.com>
2019-08-14 14:02:16 -0400

    person function

--- objects.js
+++ objects.js
@@ -21,3 +21,19 @@ function updateObject(obj) {
 const testObj = {};
 
 //console.log(updateObject(testObj));
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName:function() {
+      return this.firstName +" " +this.lastName;
+     }
+  }
+  return person;
+}
+
+const person=personMaker();
+console.log(person.fullName);
